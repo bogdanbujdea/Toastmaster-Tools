e18eca824a4b49b6ad871c1fa2f4a298ea95346e
bujdeabogdan <bujdeabogdan@gmail.com>
2015-09-29 00:09:31 +0300

    created UI for custom time picker

---
+++ ToastmastersTimer.UWP/Controls/TimePicker.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ToastmastersTimer.UWP.Controls
+{
+    using ViewModels;
+
+    public sealed partial class TimePicker
+    {
+        public TimePicker()
+        {
+            InitializeComponent();
+            Loaded += ViewLoaded;
+        }
+
+        private void ViewLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            MinutesBox.SelectedIndex = 0;
+            SecondsBox.SelectedIndex = 0;
+        }
+
+        public TimePickerViewModel ViewModel => DataContext as TimePickerViewModel;
+    }
+}
---
+++ ToastmastersTimer.UWP/ViewModels/TimePickerViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace ToastmastersTimer.UWP.ViewModels
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Mvvm;
+
+    public class TimePickerViewModel: ViewModelBase
+    {
+        private List<string> _minutes;
+        private List<string> _seconds;
+
+        public TimePickerViewModel()
+        {
+            Minutes = new List<string>();
+            Seconds = new List<string>();
+            foreach (var i in Enumerable.Range(0, 60))
+            {
+                var number = i < 10 ? "0" + i : i.ToString();
+                Minutes.Add(number);
+                Seconds.Add(number);
+            }
+        }
+
+        public List<string> Minutes
+        {
+            get { return _minutes; }
+            set
+            {
+                _minutes = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public List<string> Seconds
+        {
+            get { return _seconds; }
+            set
+            {
+                _seconds = value; 
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
