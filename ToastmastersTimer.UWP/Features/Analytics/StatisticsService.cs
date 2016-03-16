280a62b92a3761c26488ede8eff0ca5c0bfa19f6
bogdan.bujdea <bogdan@thewindev.net>
2016-03-16 12:05:02 +0200

    feature/Analytics - Integrated analytics library and added StatisticsService

---
+++ ToastmastersTimer.UWP/Features/Analytics/StatisticsService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToastmastersTimer.UWP.Features.Analytics
+{
+    public class StatisticsService : IStatisticsService
+    {
+        public StatisticsService()
+        {
+            LoadEvents();
+        }
+        public void RegisterPage(string page)
+        {
+            try
+            {
+                GoogleAnalytics.EasyTracker.GetTracker().SendView(page);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void LoadEvents()
+        {
+            EventCategories = new Dictionary<EventCategory, string>();
+            EventActions = new Dictionary<EventAction, string>();
+            EventCategories[EventCategory.UserEvent] = "user_event";
+            EventCategories[EventCategory.Timer] = "record";
+            EventActions[EventAction.StartTimer] = "start_timer";
+            EventActions[EventAction.StopTimer] = "stop_timer";
+            EventActions[EventAction.PauseTimer] = "pause_timer";
+        }
+
+        public Dictionary<EventCategory, string> EventCategories { get; set; }
+        public Dictionary<EventAction, string> EventActions { get; set; }
+
+        public void RegisterButtonPress(string button)
+        {
+            try
+            {
+                GoogleAnalytics.EasyTracker.GetTracker().SendEvent("ui_action", "button_press", button, 0);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void RegisterEvent(EventCategory category, EventAction action, string eventName)
+        {
+            try
+            {
+                GoogleAnalytics.EasyTracker.GetTracker().SendEvent(EventCategories[category], EventActions[action], eventName, 0);
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void RegisterEvent(EventCategory eventCategory, string action, string eventName)
+        {
+            try
+            {
+                GoogleAnalytics.EasyTracker.GetTracker().SendEvent(EventCategories[eventCategory], action, eventName, 0);
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+
+    public enum EventAction
+    {
+        StartTimer,
+        StopTimer,
+        PauseTimer
+    }
+
+    public enum EventCategory
+    {
+        UserEvent,
+        Timer
+    }
+}
